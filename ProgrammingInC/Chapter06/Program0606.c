fa57933faddb98336594174f7f4f69b79d92f996
Jason Tennant <jason.tennant@outlook.com>
2024-07-22 06:37:57 -0700

    updates

---
+++ ProgrammingInC/Chapter06/Program0606.c
@@ -0,0 +1,16 @@
+// Program 6.6 - Introducing Character Arrays
+
+#include <stdio.h>
+
+int main(void)
+{
+	char word[] = { 'H', 'e', 'l', 'l', 'o', '!' };
+	int i;
+
+	for (i = 0; i < 6; ++i)
+		printf("%c", word[i]);
+
+	printf("\n");
+
+	return 0;
+}
