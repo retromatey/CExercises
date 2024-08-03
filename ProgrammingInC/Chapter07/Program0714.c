58e61bcb6370b308fe8ee7f16b6da8f463917753
Jason Tennant <jason.tennant@outlook.com>
2024-08-03 08:24:36 -0700

    updates

--- ProgrammingInC/Chapter07/Program0714.c
+++ ProgrammingInC/Chapter07/Program0714.c
@@ -2,8 +2,9 @@
 
 #include <stdio.h>
 
-//void scalarMultiply(int nRows, int nCols, int matrix[nRows][nCols], int scalar)
-void scalarMultiply(int nRows, int nCols, int* matrix[], int scalar)
+#define MAX_COLUMNS 5
+
+void scalarMultiply(int nRows, int nCols, int matrix[][MAX_COLUMNS], int scalar)
 {
 	int row, column;
 
@@ -12,7 +13,7 @@ void scalarMultiply(int nRows, int nCols, int* matrix[], int scalar)
 			matrix[row][column] *= scalar;
 }
 
-void displayMatrix(int nRows, int nCols, int* matrix[])
+void displayMatrix(int nRows, int nCols, int matrix[][MAX_COLUMNS])
 {
 	int row, column;
 
@@ -27,7 +28,7 @@ void displayMatrix(int nRows, int nCols, int* matrix[])
 
 int main(void)
 {
-	int sampleMatrix[3][5] = 
+	int sampleMatrix[3][MAX_COLUMNS] =
 	{
 		{  7, 16, 55, 13, 12 },
 		{ 12, 10, 52,  0,  7 },
@@ -35,17 +36,17 @@ int main(void)
 	};
 
 	printf("Original matrix:\n");
-	displayMatrix(3, 5, sampleMatrix);
+	displayMatrix(3, MAX_COLUMNS, sampleMatrix);
 
-	scalarMultiply(3, 5, sampleMatrix, 2);
+	scalarMultiply(3, MAX_COLUMNS, sampleMatrix, 2);
 
 	printf("\nMultiplied by 2:\n");
-	displayMatrix(3, 5, sampleMatrix);
+	displayMatrix(3, MAX_COLUMNS, sampleMatrix);
 
-	scalarMultiply(3, 5, sampleMatrix, -1);
+	scalarMultiply(3, MAX_COLUMNS, sampleMatrix, -1);
 
 	printf("\nThen multiplied by -1:\n");
-	displayMatrix(3, 5, sampleMatrix);
+	displayMatrix(3, MAX_COLUMNS, sampleMatrix);
 
 	return 0;
 }
