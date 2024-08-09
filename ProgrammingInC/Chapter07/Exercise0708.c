fe3d8c0a0e65768b77ebf390f520279709efe18e
Jason Tennant <jason.tennant@outlook.com>
2024-08-09 06:58:10 -0700

    updates

--- ProgrammingInC/Chapter07/Exercise0708.c
+++ ProgrammingInC/Chapter07/Exercise0708.c
@@ -27,6 +27,16 @@
 // function that you developed in this chapter.)
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool are_equal(double a, double b, int precision)
+{
+	double factor = pow(10, precision);
+	double roundedA = round(a * factor) / factor;
+	double roundedB = round(b * factor) / factor;
+	return roundedA == roundedB;
+}
 
 // Function to calculate the absolute value
 double absoluteValue(double x)
@@ -54,28 +64,59 @@ double get_discriminant(int a, int b, int c)
 	return (double)((b * b) - 4 * a * c);;
 }
 
-void get_roots(int a, int b, int c, double* x1, double* x2)
+bool get_roots(int a, int b, int c, double* x1, double* x2)
 {
+	bool has_roots = false;
 	double discriminant = get_discriminant(a, b, c);
 
 	if (discriminant < 0)
 	{
-		printf("The roots are imaginary\n");
+		has_roots = false;
+		//printf("The roots are imaginary\n");
 	}
 	else
 	{
 		*x1 = (-b + squareRoot(discriminant)) / (2 * a);
 		*x2 = (-b - squareRoot(discriminant)) / (2 * a);
-		printf("The roots are %.3f and %.3f\n", *x1, *x2);
+		has_roots = true;
+		//printf("The roots are %.3f and %.3f\n", *x1, *x2);
 	}
+
+	return has_roots;
+}
+
+void test(int a, int b, int c, bool expected_has_roots, double expected_x1, double expected_x2)
+{
+	double x1 = 0.0;
+	double x2 = 0.0;
+	bool has_roots = get_roots(a, b, c, &x1, &x2);
+
+	bool test_passed = expected_has_roots == has_roots;
+	if (has_roots)
+	{
+		test_passed =
+			(expected_has_roots == expected_has_roots) &&
+			are_equal(expected_x1, x1, 5) && are_equal(expected_x2, x2, 5);
+	}
+
+	if (has_roots)
+		printf("a: %i, b: %i, c: %i, x1 expected: %.3f, actual: %.3f, x2 expected: %.3f, actual: %.3f, %s\n",
+			a, b, c, expected_x1, x1, expected_x2, x2, test_passed ? "PASS" : "FAIL");
+	else
+		printf("a: %i, b: %i, c: %i, imaginary roots, %s\n", a, b, c, test_passed ? "PASS" : "FAIL");
 }
 
 int main(void)
 {
-	double x1 = 0.0;
-	double x2 = 0.0;
-	get_roots(4, -17, -15, &x1, &x2);
-	// TODO: make test cases and verify logic
+	test(4, -17, -15, true, 5.0, -0.75);
+	test(1, -5, 6, true, 3.0, 2.0);
+	test(1, -8, 15, true, 5.0, 3.0);
+	test(8, -16, 0, true, 2.0, 0.0);
+	test(1, 0, -9, true, 3.0, -3.0);
+
+	// What if there's only one solution?
+	// How to handle the roots getting mixed and failing the test?
+	// Test the imaginary roots scenario
 
 	return 0;
 }
